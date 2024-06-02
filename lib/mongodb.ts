6fb5d9f5d5e9d9300893a1aac70708dd9439a72b
dasdevangshu <dasdevangshu104@gmail.com>
2024-06-03 00:23:33 +0530

    Added options for  mongo

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,12 +1,18 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-import { MongoClient } from "mongodb"
+import { MongoClient, ServerApiVersion } from "mongodb"
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
 
 const uri = process.env.MONGODB_URI
-const options = {}
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+}
 
 let client
 let clientPromise: Promise<MongoClient>
