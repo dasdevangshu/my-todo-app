51f3fedc182e2c29a8a0adfddaac3faf660c80be
dasdevangshu <dasdevangshu104@gmail.com>
2024-06-02 21:02:39 +0530

    changed vh to dvh

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
 
-      <body className={quicksand.className + ' h-screen flex flex-col'}>
+      <body className={quicksand.className + ' h-dvh flex flex-col'}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
